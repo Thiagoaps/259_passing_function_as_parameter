787d0b84e226f4ca79b57da6a79d19f5bf62541a
Thiago Antonio <thiagoapsx@gmail.com>
2024-06-09 17:41:08 -0300

    sum all

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,7 +23,9 @@ public class Program {
 		
 		// double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 		// double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'M');
-		double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+		// double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+		double sum = ps.filteredSum(list, p -> p.getPrice() > 0);
+		
 		System.out.println("Sum = " + String.format("%.2f", sum));
 
 	}
